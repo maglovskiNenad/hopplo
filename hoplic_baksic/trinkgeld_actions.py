5505b8231827238908a1720e5baead4391957045
Maglovski Nenad <79421496+maglovskiNenad@users.noreply.github.com>
2025-06-16 00:15:31 +0200

    radi

--- hoplic_baksic/trinkgeld_actions.py
+++ hoplic_baksic/trinkgeld_actions.py
@@ -1,11 +1,10 @@
-from cProfile import label
-
 import pandas as pd
 import chardet
 import re
 import customtkinter as ctk
 from tabulate import tabulate
 from tkinterdnd2 import DND_FILES, TkinterDnD
+from warning_msg import WarningPopup
 
 class TrinkgeldActions(ctk.CTkFrame,TkinterDnD.DnDWrapper):
     def __init__(self,parent,*args,**kwargs):
@@ -21,45 +20,13 @@ class TrinkgeldActions(ctk.CTkFrame,TkinterDnD.DnDWrapper):
 
         #Drag and Drop
         self.file_frame = ctk.CTkScrollableFrame(self, width=int(window_width/4), height=int(window_height/5))
-        self.file_frame.pack(side="left",fill="both",expand=True)
+        self.file_frame.pack(side="left",padx=20,fill="both",expand=True)
 
         label = ctk.CTkLabel(self, text="Drag file here", font=("Arial", 14))
         label.pack(expand=True)
 
         drop_frame = ctk.CTkFrame(self,width=int(window_width / 2),height=int(window_height / 4),corner_radius=10)
         drop_frame.pack(pady=60)
-
-        # Text box
-        df = self.load_and_clean_csv_data(
-            "data/Detailexport.csv")  # Reads a CSV file with automatic encoding detection,and cleans the column names.
-        daily_amount = self.display_and_clean_daily_tip(
-            "data/export.csv")  # Reads and cleans a daily tip amount from a tab-delimited file.
-        textbox = ctk.CTkTextbox(self, width=window_width, height=window_height, font=("Courier New", 10),
-                                 wrap="none")
-        textbox.pack(side="top", fill="both", expand=True)
-
-        # vertical scroll
-        v_scroll = ctk.CTkScrollbar(self, orientation="vertical", command=textbox.yview)
-        v_scroll.pack(side="right", fill="y")
-
-        # horizontal scroll
-        h_scroll = ctk.CTkScrollbar(self, orientation="horizontal", command=textbox.xview)
-        h_scroll.pack(side="bottom", fill="x")
-
-        # connecting them
-        textbox.configure(yscrollcommand=v_scroll.set, xscrollcommand=h_scroll.set)
-        data = self.extract_confirmed_work_hours(
-            df)  # Extracts and organizes confirmed working hours per person by date.
-        new_list_perso = self.clean_list_data(
-            data)  # Converts a nested dictionary of data into a sorted DataFrame and adds a summary row.
-        hourly_tips = self.get_hourly_tip(new_list_perso,
-                                          daily_amount)  # This script calculates the ratio between daily tips and hourly totals for a specific day.
-        calculation = self.calculation_merging_two_lists(new_list_perso,
-                                                         hourly_tips)  # Merges a DataFrame of worked hours with hourly tip values and calculates total earnings per worker
-
-        c = pd.DataFrame(calculation)
-        formatted_df = tabulate(c, headers='keys', tablefmt='grid', showindex=True)  # floatfmt=".2f"
-        textbox.insert("0.00", formatted_df)
 
         drop_frame.drop_target_register(DND_FILES)
         drop_frame.dnd_bind("<<DropEnter>>",self.on_drag_enter)
@@ -74,14 +41,51 @@ class TrinkgeldActions(ctk.CTkFrame,TkinterDnD.DnDWrapper):
         self.configure(fg_color=ctk.ThemeManager.theme["CTkFrame"]["fg_color"])
         return event.action
 
-    def handle_drop(self):
-        pass
+    def handle_drop(self,new_list):
+        if len(new_list) < 2:
+            WarningPopup(self,message="Need more file...")
+        elif len(new_list) > 2:
+            WarningPopup(self, message="We have more files then we need.")
+        elif len(new_list) == 2:
+            for data_in_list in new_list:
+                read_data = pd.read_csv(data_in_list,delimiter=";")
+                if len(read_data) > 2:
+                    df = self.load_and_clean_csv_data(data_in_list)
+                elif len(read_data) == 2:
+                    daily_amount = self.display_and_clean_daily_tip(data_in_list)
+
+            data = self.extract_confirmed_work_hours(df)
+            new_list_perso = self.clean_list_data(data)
+            hourly_tips = self.get_hourly_tip(new_list_perso, daily_amount)
+            calculation = self.calculation_merging_two_lists(new_list_perso, hourly_tips)
+
+            # Text box
+            screen_width = self.winfo_screenwidth()
+            screen_height = self.winfo_screenheight()
+
+            window_width = int(screen_width * 0.6)
+            window_height = int(screen_height * 0.6)
+            textbox = ctk.CTkTextbox(self, width=window_width, height=window_height, font=("Courier New", 10),
+                                             wrap="none")
+            textbox.pack(side="top", fill="both", expand=True)
+
+            # vertical scroll
+            v_scroll = ctk.CTkScrollbar(self, orientation="vertical", command=textbox.yview)
+            v_scroll.pack(side="right", fill="y")
+
+            # horizontal scroll
+            h_scroll = ctk.CTkScrollbar(self, orientation="horizontal", command=textbox.xview)
+            h_scroll.pack(side="bottom", fill="x")
+            textbox.configure(yscrollcommand=v_scroll.set, xscrollcommand=h_scroll.set)
+
+            # connecting them
+            c = pd.DataFrame(calculation)
+            formatted_df = tabulate(c, headers='keys', tablefmt='grid', showindex=True)  # floatfmt=".2f"
+            textbox.insert("0.00", formatted_df)
+
 
     def display_file_path(self,event):
         dropped_file = event.data.replace("{","").replace("}","")
-        #read = pd.read_csv(dropped_file,delimiter=";")
-        #l = int(len(read))
-
 
         if dropped_file:
             self.path_csv_lists.append(dropped_file)
@@ -96,7 +100,9 @@ class TrinkgeldActions(ctk.CTkFrame,TkinterDnD.DnDWrapper):
 
         new_list = list(dict.fromkeys(self.path_csv_lists))
 
+        self.handle_drop(new_list)
         return new_list
+
 
     def load_and_clean_csv_data(self,filepath,delimiter=";"):
         with open(filepath,"rb") as f:
@@ -172,7 +178,6 @@ class TrinkgeldActions(ctk.CTkFrame,TkinterDnD.DnDWrapper):
         daily_tip_amount.columns =  [clean_colums(c) for c in daily_tip_amount.columns]
 
         return daily_tip_amount
-
 
     def get_hourly_tip(self,hour,daily_tip): 
             tips = daily_tip.iloc[0].fillna(0)
---
+++ hoplic_baksic/warning_msg.py
@@ -0,0 +1,11 @@
+import customtkinter as ctk
+
+class WarningPopup(ctk.CTkToplevel):
+    def __init__(self,parent,title="Oooops...",message="Message to you"):
+        super().__init__(parent)
+        self.title(title)
+        self.geometry("300x150")
+        self.grab_set()
+
+        ctk.CTkLabel(self, text=message, wraplength=280).pack(pady=(20, 10))
+        ctk.CTkButton(self, text="Ok", command=self.destroy).pack(pady=10)
