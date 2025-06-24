54b3310a37d20ff13947c0a76d528ccc551c8697
Maglovski Nenad <79421496+maglovskiNenad@users.noreply.github.com>
2025-06-24 23:07:51 +0200

    test

---
+++ hoplic_baksic/tests.py
@@ -0,0 +1,10 @@
+import pytest
+from trinkgeld_actions import TrinkgeldActions
+from home_page import HomePage
+from warning_msg import WarningPopup
+from  main import App,SidebarFrame,MainFrame
+
+
+@pytest.fixture
+
+#Testovi
