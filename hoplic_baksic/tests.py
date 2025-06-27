975eb894616d87cfccce9641d08b1ce1065de32e
Maglovski Nenad <79421496+maglovskiNenad@users.noreply.github.com>
2025-06-27 22:21:57 +0200

    test_extract_confirmed_work_hours

--- hoplic_baksic/tests.py
+++ hoplic_baksic/tests.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pytest
 from trinkgeld_actions import TrinkgeldActions
 from home_page import HomePage
@@ -6,5 +7,30 @@ from  main import App,SidebarFrame,MainFrame
 
 
 @pytest.fixture
+def trinkgeld_obj():
+    return TrinkgeldActions(None)
 
-#Testovi
+def test_extract_confirmed_work_hours(trinkgeld_obj):
+    test_data = pd.DataFrame({
+        "tag": ["01.06.2024", "01.06.2024", "02.06.2024", "02.06.2024"],
+        "vorname":["Ana", "Marko", "Ana", "Ivan"],
+        "dauerbruttodezimal": ["4,5", "3", "5", "x"],
+        "typ": ["Bestätigte Arbeitszeit", "Bestätigte Arbeitszeit", "Unbestätigte Arbeitszeit","Bestätigte Arbeitszeit"],
+        "arbeitsbereich": ["Barista", "Barista", "Barista", "Barista"]
+    })
+
+    result = trinkgeld_obj.extract_confirmed_work_hours(test_data)
+
+    expected = {
+        "Ana": {
+            "01.06.": 4.5
+        },
+        "Marko": {
+            "01.06.": 3.0
+        },
+        "Ivan": {
+            "02.06.": 0.0
+        }
+    }
+
+    assert result == expected
